d6a5fafd9c3ed7df2ccebb2e66107170a67fc0c0
Lisavetti <lizaemets0244@gmail.com>
2025-03-17 14:01:47 +0200

    task02

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,24 +1,30 @@
 exports.handler = async (event) => {
-    console.log("Received request:", event);
 
-    const path = event.rawPath || event.path || "/";
+    let path = event.rawPath || event.path || "/";
     const method = event.requestContext?.http?.method || "UNKNOWN";
 
+    if (path.startsWith("/cmtr-1c7e15d3")) {
+        path = `/cmtr-1c7e15d3`;
+    }
+
     let response;
+
     if (path === "/hello" && method === "GET") {
         response = {
             statusCode: 200,
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ message: "Hello from Lambda" })
+            body: JSON.stringify({
+                statusCode: 200,
+                message: "Hello from Lambda"
+            })
         };
     } else {
         response = {
             statusCode: 400,
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                error: "Bad request syntax or unsupported method",
-                requestPath: path,
-                httpMethod: method
+                statusCode: 400,
+                message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
             })
         };
     }
